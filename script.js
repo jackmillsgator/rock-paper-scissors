8c59e303a0f7b1d3bc5ab299d89eefac7397c9b6
jackmillsgator <93085706+jackmillsgator@users.noreply.github.com>
2025-04-30 21:47:10 -0400

    Create playRound() and playGame()
    playGame() calls playRound() 5 times and resets both user and
    computer results before each consecutive game so that we
    don't have the same answers each time.

--- script.js
+++ script.js
@@ -41,3 +41,104 @@ function getHumanChoice()
 
 }
 
+function playRound(humanChoice, computerChoice)
+{
+
+    console.log(typeof humanChoice);
+    let localHumanChoice = humanChoice.toLowerCase();
+
+    // 1 = human wins
+    // 2 = draw
+    // 3 = computer wins
+    let winnerNum = 0;
+
+    if (humanChoice === "rock" && computerChoice === "rock")
+    {
+        winnerNum = 2;
+    }
+    else if (humanChoice === "rock" && computerChoice === "paper")
+    {
+        winnerNum = 3;
+    }
+    else if (humanChoice === "rock" && computerChoice === "scissors")
+    {
+        winnerNum = 1;
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock")
+    {
+        winnerNum = 1;
+    }
+    else if (humanChoice === "paper" && computerChoice === "paper")
+    {
+        winnerNum = 2;
+    }
+    else if (humanChoice === "paper" && computerChoice === "scissors")
+    {
+        winnerNum = 3;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "rock")
+    {
+        winnerNum = 3;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper")
+    {
+        winnerNum = 1;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "scissors")
+    {
+        winnerNum = 2;
+    }
+
+    switch (winnerNum)
+    {
+
+        case 1:
+
+            console.log("The human wins! " + humanChoice + " beats " + computerChoice);
+            break;
+
+        case 2:
+
+            console.log("It's a draw!");
+            break;
+
+        case 3:
+
+            console.log("The computer wins! " + computerChoice + " beats " + humanChoice);
+            break;
+
+    }
+
+    return winnerNum;
+
+}
+
+function playGame()
+{
+
+    let humanScore = 0;
+    let computerScore = 0;
+    let roundResults = 0;
+
+    for (let i = 1; i <= 5; i++)
+    {
+
+        humanString = getHumanChoice();
+        computerString = getComputerChoice();
+        roundResults = playRound(humanString, computerString);
+        if (roundResults === 1)
+        {
+            humanScore++;
+        }
+        else if (roundResults === 3)
+        {
+            computerScore++;
+        }
+        console.log(`humanScore = ${humanScore}. computerScore = ${computerScore}`);
+        roundResults = 0;
+
+    }
+
+}
+
+playGame();
